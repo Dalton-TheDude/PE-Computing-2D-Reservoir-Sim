af7b78a87bd0a50ed739e1219c9bb72d2e8eef1c
Dalton-TheDude <31858756+Dalton-TheDude@users.noreply.github.com>
2017-11-14 03:36:08 -0600

    Add files via upload

--- ressim.m
+++ ressim.m
@@ -1,13 +1,23 @@
+%Still need:
+%flow across each face
+%V of fluids in res
+%Pressure update
+%qcell=sum(flow across each face)
+%qcell depends on the distance to the nearest well
+%volume of fluid in each cell/whole reservoir plot
+
 x=50000;            %feet
 y=50000;            %feet
-nx=60;              
-ny=50;              
-dx=x/(nx-1);        %feet/section
-dy=y/(ny-1);        %feet/section
+thick=20;           %feet
+nx=50;              %number of cells on the x axis
+ny=50;              %number of cells on the y axis
+dx=x/(nx-1);        %feet/cell
+dy=y/(ny-1);        %feet/cell
 ndays=10;           %days
-dt=0.1;             %time step size
+dt=1;             %time step size in days
 nsteps=ndays*dt;    %total number of time steps
-p0=5000;            %psi
+p0=7000;            %psi
+bhp=2000;           %psi
 cr=0.000005;        %1/psi
 cf=0.00005;         %1/psi
 visc=250;           %cP
@@ -16,19 +26,26 @@ phi0=0.4;
 b0=1.2;             %resb/stb
 nwells=1;           
 iter=100;           %number of iterations allowed per time step
+vres=x*y*thick;     %feet^3
+vcell=dx*dy*thick;  %feet^3 per cell
 
 p=zeros(nx,ny);     
 k=zeros(nx,ny);     
 phi=zeros(nx,ny);   
 b=zeros(nx,ny);     
 y=zeros(nx,ny);     
-z=[nx,ny,p0];       
+z=[nx,ny,p0];
+peq=zeros((nx*ny),1);
+qper=zeros(nwells);
+qtot=sum(qper);
+wellloc=zeros(nwells);
+vfluid=zeros(nx,ny);
 
 %Boundary Pressure Conditions
-pwest=2000;
-peast=2000;
-pnorth=2000;
-psouth=2000;
+pwest=p0;
+peast=p0;
+pnorth=p0;
+psouth=p0;
 
 for j=1:ny
     p(1,j)=pnorth;
@@ -69,6 +86,30 @@ for i=1:nx
     end
 end
 
+for i=1:nx
+    for j=1:ny
+        vfluid(i,j)=(5.615*dx*dy*thick*phi(i,j))/(b(i,j));
+    end
+end
+
+syms p1
+phi1=phi0*exp(cr*(p1-p0));
+phip(p1)=diff(phi1,p1);
+y1=cf*(p1-p0);
+b1=b0/(1+y1+(0.5*(y1^2)));
+bp=diff(b1,p1);
+
+syms pi pii pj pjj
+xtemp1=k(i,j)/(visc*b(i,j));
+xtemp2=(pii-pi)/2;
+xterm=xtemp1*xtemp2/2;
+
+ytemp1=k(i,j)/(visc*b(i,j));
+ytemp2=(pjj-pj)/2;
+yterm=ytemp1*ytemp2/2;
+
+positionterm=xterm+yterm;
+diff(xtemp2, pi)
 
 
 
